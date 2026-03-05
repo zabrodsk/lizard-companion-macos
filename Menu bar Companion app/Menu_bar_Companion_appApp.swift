9a164a86e021705b15a6caa4afa1b51c4e8443d8
Dusan Zabrodsky <dusan.zabrodsky@gmail.com>
2026-03-05 15:34:23 +0100

    Initial Commit

---
+++ Menu bar Companion app/Menu_bar_Companion_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Menu_bar_Companion_appApp.swift
+//  Menu bar Companion app
+//
+//  Created by Dusan Zabrodsky on 05.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct Menu_bar_Companion_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
